d5a8d45d0265c2efb4ab8ea6df6d526ccdb15851
mkajnar <fotografie@mmphoto.cz>
2023-12-05 21:53:14 +0100

    Updated stop_start.sh - autocreate directories

--- stop_start.sh
+++ stop_start.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+base_directory="user_data"
+
+if [ ! -d "$base_directory" ]; then
+    mkdir $base_directory
+fi
+
+directories=(
+    "backtest_results"
+    "data"
+    "freqmodels"
+    "hyperopt_results"
+    "hyperopts"
+    "logs"
+    "notebooks"
+    "plot"
+    "strategies"
+	"data/kucoin"
+)
+
+for dir in "${directories[@]}"; do
+    if [ ! -d "$base_directory/$dir" ]; then
+        mkdir "$base_directory/$dir"
+    fi
+done
+
+chmod 777 -R user_data
+
 docker-compose down
 docker-compose up -d
 docker-compose logs -f
