2ed799824a7a074fbb246bc52043d30a4a1e996d
mkajnar <fotografie@mmphoto.cz>
2024-02-12 13:49:45 +0100

    Delete hyper.sh

--- hyper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker exec -it freqtrade freqtrade hyperopt --strategy HPStartegyDCA --config user_data/config.json --hyperopt-loss SharpeHyperOptLossDaily --spaces buy sell roi trailing stoploss -e 3000 --timerange=20230101-
