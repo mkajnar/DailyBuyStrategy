b500de96821ec6e945e077f6e3a18e7f7bd8e4f9
mkajnar <fotografie@mmphoto.cz>
2024-02-14 12:52:53 +0100

    Update HPStrategyV7.py
    Added logging for errors

--- HPStrategyV7.py
+++ HPStrategyV7.py
@@ -1,20 +1,18 @@
 import logging
+import os
+import sys
+
 import numpy as np  # noqa
 import pandas as pd  # noqa
 from pandas import DataFrame
 from typing import Optional, Union
 from freqtrade.persistence import Trade, Order
-from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter,
-                                IStrategy, IntParameter)
-# --------------------------------
-# Add your lib to import here
+from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter, IStrategy, IntParameter)
 import datetime
 import talib.abstract as ta
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 import pandas_ta as pta
 
-
-# This class is a sample. Feel free to customize it.
 class HPStrategyV7(IStrategy):
     INTERFACE_VERSION = 3
     timeframe = '5m'
@@ -28,7 +26,7 @@ class HPStrategyV7(IStrategy):
     max_entry_position_adjustment = 5
     max_dca_multiplier = 5.5
     open_trade_limit = 10
-    position_adjustment_enable = True
+    position_adjustment_enable = False
     dca_threshold_pct = DecimalParameter(0.01, 0.20, default=0.03, decimals=2, space='buy',
                                          optimize=position_adjustment_enable)
     candles_before_dca = IntParameter(1, 10, default=5, space='buy', optimize=True)
@@ -56,29 +54,39 @@ class HPStrategyV7(IStrategy):
         return self.leverage_value
 
     def calculate_heiken_ashi(self, dataframe):
-        if dataframe.empty:
-            raise ValueError("DataFrame je prázdný")
-        heiken_ashi = pd.DataFrame(index=dataframe.index)
-        heiken_ashi['HA_Close'] = (dataframe['open'] + dataframe['high'] + dataframe['low'] + dataframe['close']) / 4
-        heiken_ashi['HA_Open'] = heiken_ashi['HA_Close'].shift(1)
-        heiken_ashi['HA_Open'].iloc[0] = heiken_ashi['HA_Close'].iloc[0]
-        heiken_ashi['HA_High'] = heiken_ashi[['HA_Open', 'HA_Close']].join(dataframe['high'], how='inner').max(axis=1)
-        heiken_ashi['HA_Low'] = heiken_ashi[['HA_Open', 'HA_Close']].join(dataframe['low'], how='inner').min(axis=1)
-        # Aplikace klouzavého průměru
-        heiken_ashi['HA_Close'] = heiken_ashi['HA_Close'].rolling(window=self.rolling_ha_treshold.value).mean()
-        heiken_ashi['HA_Open'] = heiken_ashi['HA_Open'].rolling(window=self.rolling_ha_treshold.value).mean()
-        heiken_ashi['HA_High'] = heiken_ashi['HA_High'].rolling(window=self.rolling_ha_treshold.value).mean()
-        heiken_ashi['HA_Low'] = heiken_ashi['HA_Low'].rolling(window=self.rolling_ha_treshold.value).mean()
-
-        return heiken_ashi
+        try:
+            if dataframe.empty:
+                raise ValueError("DataFrame je prázdný")
+            heiken_ashi = pd.DataFrame(index=dataframe.index)
+            heiken_ashi['HA_Close'] = (dataframe['open'] + dataframe['high'] + dataframe['low'] + dataframe[
+                'close']) / 4
+            heiken_ashi['HA_Open'] = heiken_ashi['HA_Close'].shift(1)
+            heiken_ashi['HA_Open'].iloc[0] = heiken_ashi['HA_Close'].iloc[0]
+            heiken_ashi['HA_High'] = heiken_ashi[['HA_Open', 'HA_Close']].join(dataframe['high'], how='inner').max(
+                axis=1)
+            heiken_ashi['HA_Low'] = heiken_ashi[['HA_Open', 'HA_Close']].join(dataframe['low'], how='inner').min(axis=1)
+            # Aplikace klouzavého průměru
+            heiken_ashi['HA_Close'] = heiken_ashi['HA_Close'].rolling(window=self.rolling_ha_treshold.value).mean()
+            heiken_ashi['HA_Open'] = heiken_ashi['HA_Open'].rolling(window=self.rolling_ha_treshold.value).mean()
+            heiken_ashi['HA_High'] = heiken_ashi['HA_High'].rolling(window=self.rolling_ha_treshold.value).mean()
+            heiken_ashi['HA_Low'] = heiken_ashi['HA_Low'].rolling(window=self.rolling_ha_treshold.value).mean()
+            return heiken_ashi
+        except Exception as exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error('{} - {} - {}'.format(exc_type, fname, exc_tb.tb_lineno))
+            pass
+        return None
 
     def should_already_sell(self, dataframe):
         heiken_ashi = self.calculate_heiken_ashi(dataframe)
-        last_candle = heiken_ashi.iloc[-1]
-        if last_candle['HA_Close'] > last_candle['HA_Open']:
-            return False
-        else:
-            return True
+        if heiken_ashi:
+            last_candle = heiken_ashi.iloc[-1]
+            if last_candle['HA_Close'] > last_candle['HA_Open']:
+                return False
+            else:
+                return True
+        return True
 
     def adjust_entry_price(self, trade: Trade, order: Optional[Order], pair: str,
                            current_time: datetime, proposed_rate: float, current_order_rate: float,
@@ -94,25 +102,38 @@ class HPStrategyV7(IStrategy):
     def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
                            rate: float, time_in_force: str, exit_reason: str,
                            current_time: datetime, **kwargs) -> bool:
-        force_reasons = ['force_sell', 'force_exit']
-        dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
-        if 'trailing' in exit_reason:
-            return rate > trade.open_rate + self.trailing_stop_positive_offset * self.leverage_value
-        if exit_reason in force_reasons:
-            return True
-        should_already_sell = self.should_already_sell(dataframe)
-        if should_already_sell:
-            return True
+        try:
+            force_reasons = ['force_sell', 'force_exit']
+            dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
+            if 'trailing' in exit_reason:
+                return rate > trade.open_rate + self.trailing_stop_positive_offset * self.leverage_value
+            if exit_reason in force_reasons:
+                return True
+            should_already_sell = self.should_already_sell(dataframe)
+            if should_already_sell:
+                return True
+        except Exception as exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error('{} - {} - {}'.format(exc_type, fname, exc_tb.tb_lineno))
+            pass
         return False
 
     def custom_stoploss(self, pair: str, trade: 'Trade', current_time: 'datetime', current_rate: float,
                         current_profit: float, **kwargs) -> float:
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        atr = dataframe.iloc[-1]['atr']
-        atr_multiplier = 3
-        stop_loss_atr = atr * atr_multiplier
-        stop_loss_percentage = -stop_loss_atr / current_rate
-        return max(stop_loss_percentage, self.stoploss)
+        try:
+            dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+            atr = dataframe.iloc[-1]['atr']
+            atr_multiplier = 3
+            stop_loss_atr = atr * atr_multiplier
+            stop_loss_percentage = -stop_loss_atr / current_rate
+            return max(stop_loss_percentage, self.stoploss)
+        except Exception as exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error('{} - {} - {}'.format(exc_type, fname, exc_tb.tb_lineno))
+            pass
+        return self.stoploss
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe['cci'] = ta.CCI(dataframe, timeperiod=6)
@@ -140,104 +161,77 @@ class HPStrategyV7(IStrategy):
                             **kwargs) -> float:
         return proposed_stake / self.max_dca_multiplier
 
-    # Assuming you have added this attribute at the strategy class level
-    # self.last_dca_timeframe = {}
-
-    """ def adjust_trade_position(self, trade: Trade, current_time: datetime,
-                              current_rate: float, current_profit: float,
-                              min_stake: Optional[float], max_stake: float,
-                              current_entry_rate: float, current_exit_rate: float,
-                              current_entry_profit: float, current_exit_profit: float,
-                              **kwargs) -> Optional[float]:
-
-        # Ensure last_dca_time is timezone-aware, defaulting to UTC if not present
-        last_dca_time = self.last_dca_timeframe.get(trade.id,
-                                                    datetime.datetime.min.replace(tzinfo=datetime.timezone.utc))
-        if current_time - last_dca_time < datetime.timedelta(minutes=self.timeframe_to_minutes(self.timeframe)):
-            return None  # Skip DCA if already done in the current timeframe
-
-        if current_profit > self.dca_threshold_pct.value and trade.nr_of_successful_exits == 0:
-            # Take half of the profit at +5%
-            return -(trade.stake_amount / 2)
-        if current_profit > -self.dca_threshold_pct.value:
-            return None
-
-        dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
-        # Only buy when not actively falling price.
-        last_candle = dataframe.iloc[-1].squeeze()
-        previous_candle = dataframe.iloc[-2].squeeze()
-        if last_candle['close'] < previous_candle['close']:
-            return None
-
-        filled_entries = trade.select_filled_orders(trade.entry_side)
-        count_of_entries = trade.nr_of_successful_entries
-        try:
-            stake_amount = filled_entries[0].stake_amount
-            stake_amount = stake_amount * (1 + (count_of_entries * 0.25))
-            # Update the last DCA timeframe for this trade
-            self.last_dca_timeframe[trade.id] = current_time
-            return stake_amount
-        except Exception as exception:
-            return None
-        return None
-    """
-
     def adjust_trade_position(self, trade: Trade, current_time: datetime,
                               current_rate: float, current_profit: float,
                               min_stake: Optional[float], max_stake: float,
                               current_entry_rate: float, current_exit_rate: float,
                               current_entry_profit: float, current_exit_profit: float,
                               **kwargs) -> Optional[float]:
+        try:
+            dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
+            last_candle = dataframe.iloc[-1].squeeze()
+            previous_candle = dataframe.iloc[-2].squeeze()
 
-        dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
-        last_candle = dataframe.iloc[-1].squeeze()
-        previous_candle = dataframe.iloc[-2].squeeze()
+            if (current_profit > self.dca_threshold_pct.value and
+                    trade.nr_of_successful_exits == 0):
+                logging.info(f"Taking half of the profit at +5% for {trade.pair}")
+                return -(trade.stake_amount / 2)
 
-        if (current_profit > self.dca_threshold_pct.value and
-                trade.nr_of_successful_exits == 0):
-            logging.info(f"Taking half of the profit at +5% for {trade.pair}")
-            return -(trade.stake_amount / 2)
+            if trade.id in self.last_dca_timeframe.keys():
+                td = current_time - self.last_dca_timeframe[trade.id]
+                if ((td.total_seconds() / 60)
+                        < (self.timeframe_to_minutes(self.timeframe) * self.candles_before_dca.value)):
+                    return None
 
-        if trade.id in self.last_dca_timeframe.keys():
-            td = current_time - self.last_dca_timeframe[trade.id]
-            if ((td.total_seconds() / 60)
-                    < (self.timeframe_to_minutes(self.timeframe) * self.candles_before_dca.value)):
+            filled_entries = trade.select_filled_orders(trade.entry_side)
+            count_of_entries = trade.nr_of_successful_entries
+
+            if ((count_of_entries >= self.max_entry_position_adjustment) or
+                    (last_candle['close'] < previous_candle['close']) or
+                    (current_profit > -self.dca_threshold_pct.value)):
                 return None
 
-        filled_entries = trade.select_filled_orders(trade.entry_side)
-        count_of_entries = trade.nr_of_successful_entries
+            try:
+                averaged_stake = filled_entries[0].stake_amount
+            except Exception as exception:
+                averaged_stake = 5
+                pass
 
-        if ((count_of_entries >= self.max_entry_position_adjustment) or
-                (last_candle['close'] < previous_candle['close']) or
-                (current_profit > -self.dca_threshold_pct.value)):
-            return None
+            try:
+                if (count_of_entries > 0):
+                    averaged_stake = (sum([entry.stake_amount for entry in filled_entries
+                                           if entry.stake_amount > 0]) / count_of_entries)
+            except Exception as exception:
+                logging.error(f"Error calculating average stake amount: {exception}")
+                pass
 
-        try:
-            averaged_stake = filled_entries[0].stake_amount
+            stake_amount = averaged_stake * (1 + (count_of_entries * 0.25))
+            self.last_dca_timeframe[trade.id] = current_time
+            logging.info(f"Adjusted stake amount for trade {trade.id}: {stake_amount}")
+            return stake_amount
+
         except Exception as exception:
-            averaged_stake = 5
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error('{} - {} - {}'.format(exc_type, fname, exc_tb.tb_lineno))
             pass
 
-        try:
-            if (count_of_entries > 0):
-                averaged_stake = (sum([entry.stake_amount for entry in filled_entries
-                                       if entry.stake_amount > 0]) / count_of_entries)
-        except Exception as exception:
-            logging.error(f"Error calculating average stake amount: {exception}")
-            pass
-
-        stake_amount = averaged_stake * (1 + (count_of_entries * 0.25))
-        self.last_dca_timeframe[trade.id] = current_time
-        logging.info(f"Adjusted stake amount for trade {trade.id}: {stake_amount}")
-        return stake_amount
+        return None
 
     def timeframe_to_minutes(self, timeframe: str) -> int:
-        """Convert a timeframe string to minutes."""
-        if 'm' in timeframe:
-            return int(timeframe.replace('m', ''))
-        elif 'h' in timeframe:
-            return int(timeframe.replace('h', '')) * 60
-        elif 'd' in timeframe:
-            return int(timeframe.replace('d', '')) * 1440
-        else:
-            raise ValueError(f"Unsupported timeframe: {timeframe}")
+        try:
+            """Convert a timeframe string to minutes."""
+            if 'm' in timeframe:
+                return int(timeframe.replace('m', ''))
+            elif 'h' in timeframe:
+                return int(timeframe.replace('h', '')) * 60
+            elif 'd' in timeframe:
+                return int(timeframe.replace('d', '')) * 1440
+            else:
+                raise ValueError(f"Unsupported timeframe: {timeframe}")
+        except Exception as exception:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error('{} - {} - {}'.format(exc_type, fname, exc_tb.tb_lineno))
+            pass
+        return 1
