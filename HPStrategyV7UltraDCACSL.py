d051e4af2d610be92bd8286d8d5db0587a32296f
mkajnar <fotografie@mmphoto.cz>
2024-03-17 02:29:08 +0100

    Update HPStrategyV7UltraDCACSL.py
    Stable version in Drop

--- HPStrategyV7UltraDCACSL.py
+++ HPStrategyV7UltraDCACSL.py
@@ -23,7 +23,6 @@ import pandas_ta as pta
 
 
 class HPStrategyV7UltraDCACSL(IStrategy):
-
     INTERFACE_VERSION = 3
     timeframe = '5m'
     leverage_value = 3
@@ -31,60 +30,63 @@ class HPStrategyV7UltraDCACSL(IStrategy):
 
     csl = {}
     minimal_roi = {
-        "0": 0.007 * leverage_value
+        "0": 0.015 * leverage_value
     }
 
     allprofits = {}
     candle_open_prices = {}
 
-    max_open_trades = 20
+    max_open_trades = 10
 
     process_only_new_candles = True
     startup_candle_count = 50
 
     trailing_stop = True
-    trailing_only_offset_is_reached = False
-    trailing_stop_positive = 0.003 * leverage_value
-    trailing_stop_positive_offset = 0.007 * leverage_value
+    trailing_only_offset_is_reached = True
+    trailing_stop_positive = 0.006 * leverage_value
+    trailing_stop_positive_offset = 0.015 * leverage_value
 
-    use_exit_signal = False
+    use_exit_signal = True
     use_custom_stoploss = True
-    ignore_roi_if_entry_signal = False
+    ignore_roi_if_entry_signal = True
     position_adjustment_enable = True
 
-    custom_tp_pct = DecimalParameter(0.005, 0.50, default=0.005, decimals=3, space='sell', optimize=True)
+    custom_tp_pct = DecimalParameter(0.005, 0.50, default=0.005, decimals=3, space='sell', optimize=False)
 
     dca_threshold_pct_k = DecimalParameter(0.90, 0.99, default=0.94, decimals=2, space='buy',
                                            optimize=position_adjustment_enable)
-    dca_threshold_pct = DecimalParameter(0.1, 0.5, default=0.3 / leverage_value, decimals=2, space='buy',
+    dca_threshold_pct = DecimalParameter(0.1, 0.5, default=0.05, decimals=2, space='buy',
                                          optimize=position_adjustment_enable)
 
-    stoploss = dca_threshold_pct.value - 1.25
+    stoploss = (dca_threshold_pct.value - 1.25) * leverage_value
 
-    dca_multiplier = DecimalParameter(0.5, 3, default=2.71, decimals=2, space='buy',
+    dca_multiplier = DecimalParameter(1.2, 10, default=3, decimals=2, space='buy',
                                       optimize=position_adjustment_enable)
 
     dca_limit = IntParameter(1, 5, default=1, space='buy', optimize=position_adjustment_enable)
 
-    donchian_period = IntParameter(5, 50, default=23, space='buy', optimize=True)
+    donchian_period = IntParameter(5, 50, default=23, space='buy', optimize=False)
 
-    rsi_treshold = IntParameter(10, 50, default=35, space='buy', optimize=True)
+    rsi_treshold = IntParameter(10, 50, default=35, space='buy', optimize=False)
 
-    cci_treshold = IntParameter(-200, -10, default=-100, space='buy', optimize=True)
+    cci_treshold = IntParameter(-200, -10, default=-100, space='buy', optimize=False)
 
-    kick_off_threshold = DecimalParameter(-0.99, 0, default=-0.32, decimals=2, space='sell', optimize=True)
+    kick_off_threshold = DecimalParameter(-0.99, 0, default=-0.32, decimals=2, space='sell', optimize=False)
 
-    pct3_buy_threshold = DecimalParameter(-0.999, -0.005, default=-0.01, decimals=3, space='buy', optimize=True)
+    pct3_buy_threshold = DecimalParameter(-0.999, -0.005, default=-0.01, decimals=3, space='buy', optimize=False)
 
-    red_candles_before_buy = IntParameter(1, 5, default=1, space='buy', optimize=True)
+    red_candles_before_buy = IntParameter(1, 5, default=1, space='buy', optimize=False)
 
     candle_time_threshold = DecimalParameter(0.01, 1, default=0.36, decimals=2, space='buy', optimize=False)
 
-    trade_timeout = IntParameter(1, 48, default=12, space='sell', optimize=True)
+    trade_timeout = IntParameter(1, 48, default=12, space='sell', optimize=False)
 
-    max_tradable_ratio = DecimalParameter(0.01, 1, default=0.75, space='buy', optimize=True)
+    max_tradable_ratio = DecimalParameter(0.01, 1, default=0.75, space='buy', optimize=False)
 
-    max_trades = IntParameter(1, 30, default=max_open_trades, space='buy', optimize=True)
+    max_trades = IntParameter(1, 30, default=max_open_trades, space='buy', optimize=False)
+
+    price_change_1 = DecimalParameter(0.001, 0.050, default=0.003, decimals=3, space='buy', optimize=True)
+    price_change_2 = DecimalParameter(0.005, 0.150, default=0.025, decimals=3, space='buy', optimize=True)
 
     exit_profit_offset = 0.001
     exit_profit_only = False
@@ -146,6 +148,24 @@ class HPStrategyV7UltraDCACSL(IStrategy):
         dataframe = self.don_reversal(dataframe=dataframe)
         dataframe = self.don_channel_breakout(dataframe=dataframe)
         dataframe = self.calculate_price_change_coefficient(dataframe, 3)
+
+        # Calculate percentage change in price
+        dataframe['price_change'] = dataframe['close'].pct_change() * 100
+
+        # Define significant loss and recovery threshold
+        significant_loss_threshold = -5  # e.g., -5% change
+        recovery_threshold = 3  # e.g., 3% increase from previous low
+
+        # Identify significant losses
+        dataframe['significant_loss'] = dataframe['price_change'] < significant_loss_threshold
+        # Identify recoveries: where the current price has increased by a certain percentage following a significant loss
+        dataframe['recovery'] = ((dataframe['significant_loss'].shift(1) == True) &
+                                 (dataframe['price_change'] > recovery_threshold))
+        # Optional: Create a single signal combining loss and recovery for later use
+        # This is a simplified example and might need adjustment for practical use
+        dataframe['loss_and_recovery_signal'] = ((dataframe['significant_loss'] == True) &
+                                                 (dataframe['recovery'].shift(-1) == True)).astype(int)
+
         return dataframe
 
     def calc_swings(self, dataframe):
@@ -185,24 +205,31 @@ class HPStrategyV7UltraDCACSL(IStrategy):
 
         dataframe.loc[
             (
-                # ((dataframe['position_r'] == 1) |
-                #  (dataframe['swing_low'] == 1) |
-                #  (dataframe['position_m'] == 1) |
-                #  (dataframe['position_b'] == 1)) &
-                    dataframe['red_candles_in_row'] &
-                    dataframe['is_open_more_than_threshold'] &
-                    (dataframe['rsi'] < self.rsi_treshold.value) &
-                    (dataframe['cci'] < self.cci_treshold.value) &
+                    (
+                            (dataframe['position_r'] == 1) |
+                            (dataframe['swing_low'] == 1) |
+                            (dataframe['position_m'] == 1) |
+                            (dataframe['position_b'] == 1)
+                    )
+                    &
+                    (
+                            (dataframe['red_candles_in_row']) &
+                            (dataframe['is_open_more_than_threshold']) &
+                            (dataframe['rsi'] < self.rsi_treshold.value) &
+                            (dataframe['cci'] < self.cci_treshold.value)
+                    )
+                    &
                     (
                             (
-                                    (dataframe['price_change_coeff_3'] < -0.003) &
-                                    (dataframe['price_change_coeff_3'] > -0.025)
+                                    (dataframe['price_change_coeff_3'] < -self.price_change_1.value) &
+                                    (dataframe['price_change_coeff_3'] > -self.price_change_2.value)
                             )
                             |
                             (
-                                    (dataframe['price_change_coeff_3'] > 0.003) &
-                                    (dataframe['price_change_coeff_3'] < 0.025)
+                                    (dataframe['price_change_coeff_3'] > self.price_change_1.value) &
+                                    (dataframe['price_change_coeff_3'] < self.price_change_2.value)
                             )
+
                     )
 
             ), ['enter_long', 'enter_tag']
@@ -230,39 +257,82 @@ class HPStrategyV7UltraDCACSL(IStrategy):
         return dataframe
 
     def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
-                           rate: float, time_in_force: str, exit_reason: str,
-                           current_time: datetime, **kwargs) -> bool:
-
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+                                 rate: float, time_in_force: str, exit_reason: str,
+                                 current_time: datetime, **kwargs) -> bool:
+        # Removed unnecessary dataframe retrieval as it's not used in the function
         profit_ratio = trade.calc_profit_ratio(rate)
 
+        # Simplified logic by combining conditions that result in the same return value
         if 'swing' in exit_reason or 'trailing' in exit_reason:
-            confirm_pf = profit_ratio > self.exit_profit_offset
-            return confirm_pf
+            return profit_ratio > self.exit_profit_offset
 
-        if 'stop_loss' in exit_reason or 'kick_off' in exit_reason or 'force' in exit_reason:
-            return True
-
+        # For 'stop_loss', 'kick_off', 'force', or any other reason, return True
         return True
-
-
 
     def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
                         current_rate: float, current_profit: float, **kwargs) -> float:
-        try:
-            if pair in self.csl.keys():
-                o = json.loads(self.csl[pair])
-                return o.get('sl', self.stoploss)
-        except:
-            pass
-        return self.stoploss
+        stoploss_value = json.loads(self.csl.get(pair, '{}')).get('sl', self.stoploss)
+        return stoploss_value
 
     def custom_stake_amount(self, pair: str, current_time: datetime, current_rate: float,
                             proposed_stake: float, min_stake: Optional[float], max_stake: float,
                             leverage: float, entry_tag: Optional[str], side: str,
                             **kwargs) -> float:
-        max_total_stake = max_stake * self.max_tradable_ratio.value
-        return min(max_stake, max_total_stake / self.max_trades.value)
+        return min(max_stake, (max_stake * self.max_tradable_ratio.value) / (self.max_trades.value * 1.5))
+
+    # def adjust_trade_position(self, trade: Trade, current_time: datetime,
+    #                           current_rate: float, current_profit: float,
+    #                           min_stake: Optional[float], max_stake: float,
+    #                           current_entry_rate: float, current_exit_rate: float,
+    #                           current_entry_profit: float, current_exit_profit: float,
+    #                           **kwargs) -> Optional[float]:
+    #
+    #     dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
+    #     last_candle = dataframe.iloc[-1]
+    #
+    #     candle_open_price = last_candle['open']
+    #     if self.candle_open_prices.get(trade.pair, None) == candle_open_price:
+    #         return None
+    #
+    #     if trade.open_rate != current_rate:
+    #         try:
+    #             if trade.pair in self.csl.keys():
+    #                 o = json.loads(self.csl[trade.pair])
+    #                 tp = o.get('tp', None)
+    #                 if tp is not None and current_rate >= tp:
+    #                     logging.info(f"Hit {trade.pair} to TP: {self.csl[trade.pair]}, at {current_rate}")
+    #                     o['tp'] = None
+    #                     o['sl'] = None
+    #                     self.csl[trade.pair] = json.dumps(o)
+    #                     return -trade.stake_amount
+    #         except:
+    #             pass
+    #
+    #     filled_entries = trade.select_filled_orders(trade.entry_side)
+    #     if len(filled_entries) > self.dca_limit.value:
+    #         return None
+    #
+    #     new_stop_loss_price = trade.open_rate * (1 + self.stoploss)
+    #     tp_price = trade.open_rate * (1 + self.custom_tp_pct.value)
+    #     info = {
+    #         "sl": new_stop_loss_price,
+    #         "tp": tp_price
+    #     }
+    #     self.csl[trade.pair] = json.dumps(info)
+    #     # logging.info(f"Updated CSL: {self.csl[trade.pair]}")
+    #
+    #     try:
+    #         if current_rate <= (trade.open_rate * (1 - self.dca_threshold_pct.value)):
+    #             self.candle_open_prices[trade.pair] = candle_open_price
+    #             p = trade.stake_amount * self.dca_multiplier.value
+    #             # logging.info(
+    #             #     f"[ADJ] {trade.pair} current SL adjusted to {self.csl[trade.pair]}, REBUY {p} USDT")
+    #             return p
+    #
+    #     except Exception as e:
+    #         pass
+    #
+    #     return None
 
     def adjust_trade_position(self, trade: Trade, current_time: datetime,
                               current_rate: float, current_profit: float,
@@ -272,48 +342,27 @@ class HPStrategyV7UltraDCACSL(IStrategy):
                               **kwargs) -> Optional[float]:
 
         dataframe, _ = self.dp.get_analyzed_dataframe(trade.pair, self.timeframe)
-        last_candle = dataframe.iloc[-1]
+        if len(dataframe) < 3:
+            return None
 
+        last_candle, second_last_candle, third_last_candle = dataframe.iloc[-1], dataframe.iloc[-2], dataframe.iloc[-3]
         candle_open_price = last_candle['open']
-        if self.candle_open_prices.get(trade.pair, None) == candle_open_price:
+
+        if self.candle_open_prices.get(trade.pair) == candle_open_price:
             return None
 
-        if trade.open_rate != current_rate:
-            try:
-                if trade.pair in self.csl.keys():
-                    o = json.loads(self.csl[trade.pair])
-                    tp = o.get('tp', None)
-                    if tp is not None and current_rate >= tp:
-                        logging.info(f"Hit {trade.pair} to TP: {self.csl[trade.pair]}, at {current_rate}")
-                        o['tp'] = None
-                        o['sl'] = None
-                        self.csl[trade.pair] = json.dumps(o)
-                        return -trade.stake_amount
-            except:
-                pass
+        self.candle_open_prices[trade.pair] = candle_open_price
 
-        filled_entries = trade.select_filled_orders(trade.entry_side)
-        if len(filled_entries) > self.dca_limit.value:
-            return None
+        is_last_candle_green = last_candle['close'] > last_candle['open']
+        is_previous_candles_red = all(
+            candle['close'] < candle['open'] for candle in [second_last_candle, third_last_candle])
 
-        new_stop_loss_price = trade.open_rate * (1 + self.stoploss)
-        tp_price = trade.open_rate * (1 + self.custom_tp_pct.value)
-        info = {
-            "sl": new_stop_loss_price,
-            "tp": tp_price
-        }
-        self.csl[trade.pair] = json.dumps(info)
-        # logging.info(f"Updated CSL: {self.csl[trade.pair]}")
+        # Make threshold dynamic and multiplied by leverage
+        dynamic_threshold = -self.dca_threshold_pct.value * self.leverage_value
 
-        try:
-            if current_rate <= (trade.open_rate * (1 - self.dca_threshold_pct.value)):
-                self.candle_open_prices[trade.pair] = candle_open_price
-                p = trade.stake_amount * self.dca_multiplier.value
-                # logging.info(
-                #     f"[ADJ] {trade.pair} current SL adjusted to {self.csl[trade.pair]}, REBUY {p} USDT")
-                return p
-
-        except Exception as e:
-            pass
+        if current_profit <= dynamic_threshold and is_last_candle_green and is_previous_candles_red:
+            logging.info(f'{current_time} Adjusting position for {trade.pair} with {trade.stake_amount}. '
+                         f'Last two candles were red, and current candle is green.')
+            return min(max_stake, trade.stake_amount * self.dca_multiplier.value)
 
         return None
